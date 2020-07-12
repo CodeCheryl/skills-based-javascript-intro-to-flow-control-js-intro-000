e11678de873773775cad732fc95184547dcf73b1
Code Cheryl <cheryl.morgenstern@gmail.com>
2020-07-12 19:15:44 +0000

    Automatically backed up by Learn

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -111,9 +111,11 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case age >= 13 && age <= 19 :
+    case age >= 13:
     return "You are a teenager";
     break;
+    case age <=19;
+    return "You are a teenager";
     default:
     return "You have an age";
   }
